0c58bf592e01f631202b04cef61b0abef96045b5
Sujay Alaspure <sujay.alaspure@robosoftin.com>
2022-06-12 21:06:44 +0530

    :wrench:

---
+++ background.js
@@ -0,0 +1,17 @@
+console.log("background.js loaded");
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  console.log("tabstatus->> ", tab.status);
+  chrome.tabs.sendMessage(tabId, {
+    message: "tabstatus",
+    tabstatus: tab.status,
+    url: tab.url,
+  });
+  // if (tab.status === "complete" && changeInfo.url) {
+  //   if (/(http|https):\/\/www.scaler.com\/academy\/.*\/problems\/[0-9]+.*/.test(changeInfo.url)) {
+  //     console.log("The current url is correct");
+  //   } else {
+  //     console.log("The current url is incorrect");
+  //   }
+  // }
+});
---
+++ content.js
@@ -0,0 +1,91 @@
+var currentUrl = window.location.href;
+let flag = false;
+if (/(http|https):\/\/www.scaler.com\/academy\/.*\/problems\/[0-9]+.*/.test(currentUrl)) {
+  console.log("main correct");
+  flag = false;
+} else {
+  console.log("main incorrect");
+}
+
+// Main code start here
+
+chrome.runtime.onMessage.addListener(function (response, sendResponse) {
+  // console.log("Mesage->> ", response);
+
+  if (/(http|https):\/\/www.scaler.com\/academy\/.*\/problems\/[0-9]+.*/.test(response.url)) {
+    flag = false;
+    if (response.tabstatus === "complete") {
+      run("messaage ");
+    } else {
+      window["onload"] = function () {
+        run("message1");
+      };
+    }
+  } else {
+    console.log("from msg incorrect url");
+  }
+  // return true;
+});
+
+function stopWatch() {
+  var playPause = document.querySelector("#playpause");
+  var countdown = document.querySelector("#timer");
+  let time = 0;
+  let gate = false;
+
+  const intervalFn = function () {
+    countdown.innerHTML = new Date(time * 1000).toISOString().substr(11, 8);
+    time++;
+  };
+
+  let updatecountDown = setInterval(intervalFn, 1000);
+
+  playPause.addEventListener("mouseup", function () {
+    lala = 0;
+    if (gate == false) {
+      clearInterval(updatecountDown);
+      playPause.innerHTML = `Play`;
+      gate = true;
+    } else {
+      updatecountDown = setInterval(intervalFn, 1000);
+      playPause.innerHTML = `Pause`;
+      gate = false;
+    }
+  });
+}
+
+function run(from = "main") {
+  // console.log(from, "run");
+  // let parentElement = null;
+
+  const htmlText = `
+      <button class="tappable btn btn-primary btn-small btn-long m-r-5" id="playpause">Pause</button>
+      <div class="row bold ">
+        <div class="cr-problem-user-score">
+          <div class="flex row flex-ac h5 no-mgn-b">
+            <i class="cr-icon-clock m-r-5"></i><span id="timer">275</span>
+          </div>
+        </div>
+      </div>`;
+
+  let parentElement = null;
+  var checkExist = setInterval(function () {
+    parentElement = document.querySelector(".cr-problem-header");
+    if (flag === false && parentElement) {
+      flag = true;
+      console.log("Exists!");
+      clearInterval(checkExist);
+      const currentThirdNode = parentElement.childNodes[1];
+      const newNode = document.createElement("div");
+      newNode.innerHTML = htmlText;
+      newNode.style.cssText = "margin-right: 10px; display: flex; flex-direction: row;";
+      // newNode.style.marginRight = "20px";
+      // newNode.style.display = "flex";
+      // newNode.style.flexDirection = "row";
+      // newNode.style.alignItems = "center";
+
+      parentElement.insertBefore(newNode, currentThirdNode);
+      stopWatch();
+    }
+  }, 200); // check every 100ms
+}
