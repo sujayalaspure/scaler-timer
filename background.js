a80a097d2bc6873d5f2541373504b0d1d37e0f87
Sujay Alaspure <sujay.alaspure@robosoftin.com>
2022-06-26 20:24:27 +0530

    Added Threshold notice

--- background.js
+++ background.js
@@ -1,17 +1,10 @@
 console.log("background.js loaded");
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  console.log("tabstatus->> ", tab, changeInfo);
+  // console.log("tabstatus->> ", tab, changeInfo);
   chrome.tabs.sendMessage(tabId, {
     message: "tabstatus",
     tabstatus: tab.status,
     url: tab.url,
   });
-  // if (tab.status === "complete" && changeInfo.url) {
-  //   if (/(http|https):\/\/www.scaler.com\/academy\/.*\/problems\/[0-9]+.*/.test(changeInfo.url)) {
-  //     console.log("The current url is correct");
-  //   } else {
-  //     console.log("The current url is incorrect");
-  //   }
-  // }
 });
--- content.js
+++ content.js
@@ -1,24 +1,24 @@
-var currentUrl = window.location.href;
 let flag = false;
-if (/(http|https):\/\/www.scaler.com\/academy\/.*\/problems\/[0-9]+.*/.test(currentUrl)) {
-  console.log("main correct");
-  flag = false;
-} else {
-  console.log("main incorrect");
-}
+let noticeFlag = false;
+const TIME_THRESHOLD = 2700; // seconds
+
+const bannerElement = `
+  <div class="banner__icon cr-banner__icon cr-banner__icon--compact"> <div class="mascot-emoji mascot-emoji--small"> <img class="mascot-emoji__bg" src="https://assets.scaler.com/packs/images/bg-red.6e3c81.svg" alt="character background"/><img class="mascot-emoji__mascot" src="https://assets.scaler.com/packs/images/love.4dd60c.svg" alt="character"/> </div></div><div class="banner__body cr-banner__body"> <div class="banner__title"> <div class="cr-banner-title cr-banner-title--small m-b-5">You are solving this for 45 mins</div></div><div class="banner__subtitle cr-banner__subtitle cr-banner__subtitle--small"> Take a small 5 Min break and come back stronger. <br/> Trust me, Fod donge is baar. </div></div><a class="tappable" id="noticeclose" ><i class="icon-close"></i></a>
+`;
+
+const cssText = `position: absolute;top: 50px;right: 10px;z-index: 10;display: none;`;
 
 // Main code start here
+let parentElement = null;
 
 chrome.runtime.onMessage.addListener(function (response, sendResponse) {
-  // console.log("Mesage->> ", response.url);
-
   if (/(http|https):\/\/www.scaler.com\/academy\/.*\/problems\/[0-9]+.*/.test(response.url)) {
     flag = false;
     if (response.tabstatus === "complete") {
-      run("messaage ");
+      run();
     } else {
       window["onload"] = function () {
-        run("message1");
+        run();
       };
     }
   } else {
@@ -26,6 +26,19 @@ chrome.runtime.onMessage.addListener(function (response, sendResponse) {
   }
   // return true;
 });
+
+function Notice() {
+  let noticeRef = document.querySelector("#notice");
+  let noticeclose = document.querySelector("#noticeclose");
+  if (noticeRef) {
+    if (noticeFlag === false) {
+      noticeRef.style.display = "flex";
+    }
+    noticeclose.addEventListener("mouseup", function () {
+      noticeRef.style.display = "none";
+    });
+  }
+}
 
 function stopWatch() {
   var playPause = document.querySelector("#playpause");
@@ -36,12 +49,15 @@ function stopWatch() {
   const intervalFn = function () {
     countdown.innerText = new Date(time * 1000).toISOString().substr(11, 8);
     time++;
+    if (time >= TIME_THRESHOLD) {
+      Notice();
+      noticeFlag = true;
+    }
   };
 
   let updatecountDown = setInterval(intervalFn, 1000);
 
   playPause.addEventListener("mouseup", function () {
-    lala = 0;
     if (gate == false) {
       clearInterval(updatecountDown);
       playPause.innerHTML = `Play`;
@@ -54,21 +70,9 @@ function stopWatch() {
   });
 }
 
-function run(from = "main") {
-  // console.log(from, "run");
-  // let parentElement = null;
+function run() {
+  const htmlText = `<button class="tappable cr-judge-action btn btn-primary btn-small btn-long m-r-5" id="playpause">Pause</button> <div class="row bold "> <div class="cr-problem-user-score"> <div class="flex row flex-ac h5 no-mgn-b"> <i class="cr-icon-clock m-r-5"></i><span id="timer">00:00:00</span> </div></div></div>`;
 
-  const htmlText = `
-      <button class="tappable btn btn-primary btn-small btn-long m-r-5" id="playpause">Pause</button>
-      <div class="row bold ">
-        <div class="cr-problem-user-score">
-          <div class="flex row flex-ac h5 no-mgn-b">
-            <i class="cr-icon-clock m-r-5"></i><span id="timer">00:00:00</span>
-          </div>
-        </div>
-      </div>`;
-
-  let parentElement = null;
   var checkExist = setInterval(function () {
     parentElement = document.querySelector(".cr-problem-header");
     if (flag === false && parentElement) {
@@ -80,12 +84,16 @@ function run(from = "main") {
       newNode.id = "timer-container";
       newNode.innerHTML = htmlText;
       newNode.style.cssText = "margin-right: 10px; display: flex; flex-direction: row;";
-      // newNode.style.marginRight = "20px";
-      // newNode.style.display = "flex";
-      // newNode.style.flexDirection = "row";
-      // newNode.style.alignItems = "center";
+
+      const NoticeNode = document.createElement("div");
+      NoticeNode.id = "notice";
+      NoticeNode.className = "banner cr-banner cr-banner--secondary cr-banner--small cr-banner--compact m-10";
+      NoticeNode.style.cssText = cssText;
+      NoticeNode.innerHTML = bannerElement;
+
       const hasChild = parentElement.querySelector("#timer-container") != null;
       if (!hasChild) {
+        parentElement.insertBefore(NoticeNode, currentThirdNode);
         parentElement.insertBefore(newNode, currentThirdNode);
         stopWatch();
       }
